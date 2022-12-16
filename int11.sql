f8a65a6b85b04224fe902351bb77b607c5fa8b26
diogotvf7 <up202108752@g.uporto.pt>
2022-12-16 20:44:17 +0000

    Moved int11 to int4

--- int11.sql
+++
@@ -1,9 +0,0 @@
-select case 
-            when minuto not like "%+%" then cast(substr(minuto, 1, length(minuto) - 1) as int)
-            when minuto like "%+%" then cast(substr(minuto, 1, 2) as int)
-        end as MINUTO_REGULAMENTAR,
-        case when minuto like "%+%" then cast(substr(minuto, 4, length(minuto) - 4) as int) end as MINUTO_COMPENSACAO,
-		count(idGolo) as GOLOS
-from golo
-group by minuto
-order by 1, 2
