0b83a0b90d4dc0a4b1a3a525d5f05b726f3dbaec
Jaime Francisco <jaimefrancisco200343@gmail.com>
2022-12-19 10:49:17 +0000

    New query in int6

--- int6.sql
+++ int6.sql
@@ -1,3 +1,11 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+/* listar relação entre golos marcados no prolongamento em relação a golos em
+tempo regulamentar para todas as equipas */
+
+select a.GolosProlongamento * 100/b.GolosTotais as result, a.nomeClube from 
+(select count(*) as GolosProlongamento, CLUBE.idClube as clubeID, CLUBE.nome as nomeClube from GOLO, JOGADOR, CLUBE where golo.idMarcador = JOGADOR.idJogador and JOGADOR.idClube = CLUBE.idClube and golo.minuto like '90+%' group by 2) as a, 
+(select count(*) as GolosTotais, CLUBE.idClube as clubeID from clube, GOLO, JOGADOR where golo.idMarcador = JOGADOR.idJogador and JOGADOR.idCLUBE = CLUBE.idClube group by 2) as b where a.clubeID = b.clubeID
+order by 1 DESC
