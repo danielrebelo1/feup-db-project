2992fb770f006e728a478e27e949c30295df7ad0
danielrebelo1 <daniel.rebelo2003@gmail.com>
2022-12-19 11:24:47 +0000

    replaced the querie on int2.sql

--- int2.sql
+++ int2.sql
@@ -2,9 +2,16 @@
 .headers on
 .nullvalue NULL
 
-/* listar todos os avancados da liga */
-
-select j.nome AS NOME , j.nacionalidade as NACIONALIDADE , c.nome as CLUBE 
-from jogador j join clube c using (idClube)
-where j.posicao = 'A' 
-order by 1;
+/* equipas que estejam no top 3 melhores ataques e top 3 melhores defesas */
+select nome 
+from (select CLUBE.NOME as nome , count(*) as GOLOS_MARCADOS from clube, GOLO, JOGADOR where golo.idMarcador = JOGADOR.idJogador and JOGADOR.idCLUBE = CLUBE.idClube group by 1
+order by 2 desc
+limit 3) intersect 
+select nome from (select clube.nome nome, a.counter  + b.counter AS GOLOS_SOFRIDOS
+FROM (select clube.idClube id , count(*) as counter  from golo , jogo , clube , jogador
+where golo.idJogo = jogo.idJogo and jogo.idEquipaVisitada = clube.idClube and GOLO.idMarcador = jogador.idJogador and jogador.idClube <> clube.idClube
+group by clube.idClube) as a , (select clube.idClube id, count(*)as counter  from golo , jogo , clube , jogador
+where golo.idJogo = jogo.idJogo and jogo.idEquipaVisitante = clube.idClube and GOLO.idMarcador = jogador.idJogador and jogador.idClube <> clube.idClube
+group by clube.idClube) as b join clube where a.id = b.id and a.id = clube.idClube
+ORDER BY 2
+limit 3)
--- trigger1.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER updateResultados 
-on JOGO
-AFTER INSERT
-FOR EACH ROW
-BEGIN
-    WHEN
-        EXISTS (SELECT 1 FROM jogo j , clube c WHERE j.idEquipaVisitada = clube.idClube)
-END;
-
