73b303067be4b2ff67adfe9eb42a4824a5dcc895
Jaime Francisco <jaimefrancisco200343@gmail.com>
2022-12-19 10:51:05 +0000

    Correction on query description

--- int6.sql
+++ int6.sql
@@ -2,8 +2,7 @@
 .headers on
 .nullvalue NULL
 
-/* listar relação entre golos marcados no prolongamento em relação a golos em
-tempo regulamentar para todas as equipas */
+/* listar relação entre golos marcados no prolongamento em relação a golos totais para todas as equipas */
 
 select a.GolosProlongamento * 100/b.GolosTotais as result, a.nomeClube from 
 (select count(*) as GolosProlongamento, CLUBE.idClube as clubeID, CLUBE.nome as nomeClube from GOLO, JOGADOR, CLUBE where golo.idMarcador = JOGADOR.idJogador and JOGADOR.idClube = CLUBE.idClube and golo.minuto like '90+%' group by 2) as a, 
