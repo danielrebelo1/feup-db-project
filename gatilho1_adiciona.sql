a3e2ed3997ab74ccea9cc3b88bf39640cd21c7c2
danielrebelo1 <daniel.rebelo2003@gmail.com>
2022-12-19 20:03:58 +0000

    added my trigger

--- gatilho1_adiciona.sql
+++ gatilho1_adiciona.sql
@@ -4,10 +4,11 @@ for each row
 begin
     update classificacao
         set goalsScored = goalsScored + 1
-        where idClube = (select clube.idClube from golo join jogo on new.idJogo = jogo.idJogo join jogador on new.idMarcador = jogador.idJogador join clube on jogador.idClube = clube.idClube);
+        where idClube = (select jogador.idClube from jogador join golo where jogador.idJogador = new.idMarcador);
     update classificacao
         set goalsConceded = goalsConceded + 1
-        where idClube = (select clube.idClube from golo join jogo on new.idJogo = jogo.idJogo join jogador on new.idMarcador = jogador.idJogador join clube on jogador.idClube = clube.idClube
-        where idClube <> (select clube.idClube from golo join jogo on new.idJogo = jogo.idJogo join jogador on new.idMarcador = jogador.idJogador join clube on jogador.idClube = clube.idClube));
+        where idClube = (select clube.idClube from jogo,clube where jogo.idJogo = new.idJogo and idClube <> (select jogador.idClube from jogador join golo where jogador.idJogador = new.idMarcador) and idClube in 
+			(select idEquipaVisitada as Equipas from jogo where idJogo = new.idJogo
+			UNION
+			select idEquipaVisitante from jogo where idjogo = new.idJogo));
 end;
-
--- gatilho1_verifica.sql
+++ gatilho1_verifica.sql
@@ -1,5 +1,5 @@
-select goalsScored , goalsConceded from classificacao where idClube = 7;
+select goalsScored , goalsConceded , idClube from classificacao group by idClube ;
 
 insert into golo(idGolo,minuto,idMarcador,idAssistente,idJogo) values (906,'10''',438,92,2);
 
-select goalsScored , goalsConceded from classificacao where idClube = 7;
+select goalsScored , goalsConceded ,idClube from classificacao group by idClube ;
