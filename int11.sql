5c2c2cc93a99e5fdf217a1b6847f6c2dfc89690f
diogotvf7 <up202108752@g.uporto.pt>
2022-12-20 22:45:00 +0000

    Improved int11.sql

--- int11.sql
+++ int11.sql
@@ -2,21 +2,23 @@
 .headers on
 .nullvalue NULL
 
-/* lista dos jogadores estrangeiros por equipa com maior número de golos+assistencias
-   (a última coluna indica se este jogador e tambem o melhor tendo em conta os jogadores brasileiros da equipa) */
+/* lista dos jogadores estrangeiros por equipa com maior número de golos+assistências entre as jornadas 10 (exclusivé) e 20 (inclusivé)
+   (a última coluna indica se este jogador é - independentemente da nacionalidade- da equipa neste intervalo de jornadas) 
+   ordenação por número de golos+assistências e desempate por nacionalidade								*/
 
 SELECT EQUIPA, NOME_JOGADOR, NACIONALIDADE_JOGADOR, MAX(TMP) GOLOS_MAIS_ASSISTENCIAS, (CASE WHEN MAX(TMP) = (SELECT MAX(G_A)
-																											FROM (SELECT COUNT(*) G_A
-																											FROM GOLO JOIN JOGADOR JOIN CLUBE
-																											ON (GOLO.idMarcador = JOGADOR.idJogador OR GOLO.idAssistente = JOGADOR.idJogador) AND JOGADOR.idClube = CLUBE.idClube 
-																											GROUP BY idJogador)
-																											) THEN 'SIM' ELSE 'NAO' END) AS MELHOR_DA_EQUIPA
+																												FROM (SELECT COUNT(*) G_A
+																												FROM JOGO JOIN GOLO JOIN JOGADOR JOIN CLUBE
+																												ON JOGO.idJogo = GOLO.idJogo AND (GOLO.idMarcador = JOGADOR.idJogador OR GOLO.idAssistente = JOGADOR.idJogador) AND JOGADOR.idClube = CLUBE.idClube
+																												WHERE CLUBE.idClube = ID AND 10 < idJornada AND idJornada <= 20
+																												GROUP BY idJogador)
+																												) THEN 'SIM' ELSE 'NAO' END) AS MELHOR_DA_EQUIPA
 FROM (
-		SELECT CLUBE.nome EQUIPA, JOGADOR.nome NOME_JOGADOR, nacionalidade NACIONALIDADE_JOGADOR, COUNT(*) TMP
-		FROM GOLO JOIN JOGADOR JOIN CLUBE
-		ON (GOLO.idMarcador = JOGADOR.idJogador OR GOLO.idAssistente = JOGADOR.idJogador) AND JOGADOR.idClube = CLUBE.idClube
-		WHERE nacionalidade <> 'Brasil'
+		SELECT CLUBE.idClube ID, CLUBE.nome EQUIPA, JOGADOR.nome NOME_JOGADOR, nacionalidade NACIONALIDADE_JOGADOR, COUNT(*) TMP
+		FROM JOGO JOIN GOLO JOIN JOGADOR JOIN CLUBE
+		ON JOGO.idJogo = GOLO.idJogo AND (GOLO.idMarcador = JOGADOR.idJogador OR GOLO.idAssistente = JOGADOR.idJogador) AND JOGADOR.idClube = CLUBE.idClube
+		WHERE nacionalidade <> 'Brasil' AND 10 < idJornada AND idJornada <= 20
 		GROUP BY idJogador
 		)
-GROUP BY 1
-ORDER BY 4 DESC, 2
+GROUP BY ID
+ORDER BY 4 DESC, 3
