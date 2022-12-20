faab23549da5efc95ed7345f068d4cc26dbbac8a
danielrebelo1 <daniel.rebelo2003@gmail.com>
2022-12-20 17:31:55 +0000

    updated some data on povoar.sql

--- int8.sql
+++ int8.sql
@@ -4,10 +4,9 @@
 
 /* jogadores da equipa do presidente + velho c mais de 1.75 de altura */
 
-select jogador.nome , idClube , jogador.altura from 
-jogador join
-(select idClube , NOME , max(IDADE) from 
-(select p.idPresidente as ID, p.idClube as idClube,  p.nomePresidente as NOME , strftime('%Y','now') - substr(p.dataNascimento,7,4) as IDADE from presidente p)
-order by 2 desc , 1 asc) using(idClube)
-where substr(jogador.altura,1,4) >= 1.75
+select jogador.nome as NOME_JOGADOR , jogador.altura as ALTURA_JOGADOR from jogador join
+(select idClube, NOMECLUBE,  NOME , max(IDADE) from 
+(select p.idPresidente as ID, p.idClube as idClube, c.nome as NOMECLUBE, p.nomePresidente as NOME , strftime('%Y','now') - substr(p.dataNascimento,7,4) as IDADE 
+from presidente p join clube c using(idClube))) using(idClube) join clube using(idClube)
+where substr(jogador.altura,1,4) >= 1.75 
 order by substr(jogador.altura,1,4) desc , 1 asc
