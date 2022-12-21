67c8340f174c9c258d27d7f4e0afbc00992a9f2d
diogotvf7 <up202108752@g.uporto.pt>
2022-12-21 20:00:51 +0000

    Switched points to commas on players heights and fixed int8 by casting string to float

--- int/int8.sql
+++ int/int8.sql
@@ -8,5 +8,5 @@ select jogador.nome as NOME_JOGADOR , jogador.altura as ALTURA_JOGADOR from joga
 (select idClube, NOMECLUBE,  NOME , max(IDADE) from 
 (select p.idPresidente as ID, p.idClube as idClube, c.nome as NOMECLUBE, p.nomePresidente as NOME , strftime('%Y','now') - substr(p.dataNascimento,7,4) as IDADE 
 from presidente p join clube c using(idClube))) using(idClube) join clube using(idClube)
-where substr(jogador.altura,1,4) >= 1.75 
-order by substr(jogador.altura,1,4) desc , 1 asc
+where CAST(substr(jogador.altura,1,4) AS FLOAT) >= 1.75 
+order by CAST(substr(jogador.altura,1,4) AS FLOAT) desc , 1 asc
