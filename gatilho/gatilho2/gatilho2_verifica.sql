c0007a4bbb57b4bf62013fcb58e62caef0133102
diogotvf7 <up202108752@g.uporto.pt>
2022-12-21 19:10:51 +0000

    Test gatilho2

--- gatilho/gatilho2/gatilho2_verifica.sql
+++ gatilho/gatilho2/gatilho2_verifica.sql
@@ -1,3 +1,3 @@
 SELECT * from jogo limit 1;
-insert into jogo(idJogo, dataJogo, horaJogo, assistencia, idArbitro, idEstadio, idEquipaVisitada, idEquipaVisitante, idJornada) values (1,'2022-04-09','20:30',22855,3,7,9,15,1);
+insert into jogo(idJogo, dataJogo, horaJogo, assistencia, idArbitro, idEstadio, idEquipaVisitada, idEquipaVisitante, idJornada) values (381,'2022-04-09','20:30',22855,3,7,9,15,36);
 SELECT * from jogo limit 1;
